e47bd7bca26666a1b3154de2ce5d89aaaf967ca6
kassama fally <fally.kassama@outlook.com>
2019-10-15 16:54:51 +0200

    ajout du nom

--- partie1.py
+++ partie1.py
@@ -3,9 +3,15 @@ print ("hello world")
 variable = "hello world"
 print(variable)
 
+#ex 2 afficher resultat
 print(3*3, 4+9+78, 12-7, 5+4)
 
 try:
     print(12/0)
 except ZeroDivisionError:
     print("pas divisible par 0")
+
+
+# ex 3 communiqué avec l'ordinateur
+nom = input("merci d'indiquer votre nom")
+print("bienvenue" +""+ nom)

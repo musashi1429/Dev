351323eb0051b83699f54d04b557d47e22b8e192
kassama fally <fally.kassama@outlook.com>
2019-10-16 11:25:27 +0200

    exercice 1 partie2

---
+++ partie2.py
@@ -0,0 +1,20 @@
+##1\.Exercice 1 : True ou False ?
+#Déclarer 2 variables de type String. L'une d'entre elles doit être vide.
+#Ecrire un programme vérifiant si chacune des deux variables est vide ou non, et renvoyer la valeur (True ou False) dans le terminal selon le cas de figure.
+#Pour cet exercice, vous devrez commencer à vous renseigner par vous-même sur les conditions en Python.
+
+a = ""
+b = "non vide"
+
+nombrea = len(a)
+nombreb = len(b)
+
+if nombrea == 0:
+    print(nombrea == 0)
+else:
+    print(nombrea == 0)
+
+if nombreb == 0:
+    print(nombreb == 0)
+else:
+    print(nombreb == 0)

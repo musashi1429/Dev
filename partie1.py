68dec3a263f5a911000a1f027543f3a42498707e
kassama fally <fally.kassama@outlook.com>
2019-10-16 10:02:14 +0200

    fin

--- partie1.py
+++ partie1.py
@@ -9,19 +9,20 @@ print(3*3, 4+9+78, 12-7, 5+4)
 try:
     print(12/0)
 except ZeroDivisionError:
-    print("pas divisible par 0")
+    print("12 n'est pas divisible par 0")
 
 
 # ex 3 communiqué avec l'ordinateur
-B = input("merci d'indiquer votre nom")
+B = input("merci d'indiquer votre nom ")
 print("bienvenue" +" "+ B)
 
+#ex 4
 nom = "kassama"
 prenom = "fally"
 print("bonjour" +" "+nom+" "+prenom)
 print("bonjour {} {}".format(nom, prenom))
 
-
+#ex 5
 MyNumber = "123"
 
 MyNumber = int(MyNumber)
@@ -30,6 +31,6 @@ print(type(MyNumber))
 nom = str(nom)
 prenom = str(prenom)
 
-
+# ex 6
 print(nom.upper())
 print(prenom.lower())

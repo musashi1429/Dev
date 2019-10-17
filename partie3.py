2f630d4c3a59568e6873fd72370060b31337e45b
kassama fally <fally.kassama@outlook.com>
2019-10-17 12:42:00 +0200

    exercice 3

--- partie3.py
+++ partie3.py
@@ -10,32 +10,33 @@ list = [a,b,c,d]
 print(max(list))
 
 ##Exercice 2
-#import math
-#user = ""
-#while user != "stop":
-#    print("entrer votre age")
-#    Age = int(input())
-#    if Age <= 0:
-#        print("merci d'indiquer un age réel")
-#    if Age >= 21:
-#        print("vous étes autorisé a accéder au portail")
-#    if Age%2 == 0:
-#        print("age pair,mhh,chanceux!!!")
-#    if math.sqrt(Age).is_integer():
-#        print("votre age et un carré")
-#    user = str(input("si vous voulez quittez tapez stop ou entrer pour continuer"))
+import math
+user = ""
+while user != "stop":
+    print("entrer votre age")
+    Age = int(input())
+    if Age <= 0:
+        print("merci d'indiquer un age réel")
+    if Age >= 21:
+        print("vous étes autorisé a accéder au portail")
+    if Age%2 == 0:
+        print("age pair,mhh,chanceux!!!")
+    if math.sqrt(Age).is_integer():
+        print("votre age et un carré")
+    user = str(input("si vous voulez quittez tapez stop ou entrer pour continuer"))
 
 
 ##Exercice 3
 v = random.randint(1,11)
-u = int(input())
+u = int(input("trouver le chiffre au hasard: "))
 while u != v:
      if u < v:
          print("trop bas mon petit")
-         u=int(input())
+         u=int(input("allez, un autre"))
      if u > v:
          print("trop haut mon grand")
-         u=int(input())
+         u=int(input("allez, un autre"))
      if u == v:
          print("petit ou grand malin,Bravo!!!")
-         u=int(input())
+         u=int(input("trouver le chiffre au hasard: "))
+    if 

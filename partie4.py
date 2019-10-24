6e1e13ac5cfc3fe945f5096a3090d45c69571fff
kassama fally <fally.kassama@outlook.com>
2019-10-24 11:10:52 +0200

    exercice insert

---
+++ partie4.py
@@ -0,0 +1,60 @@
+## 1\.Exercice 1 : Un échiquier ma logique a peaufinner.
+#i = "# "
+#b = " #"
+#echiquier = ""
+#while len(echiquier) <= 73:
+#    echiquier.append(b)
+#if echiquier.append(b)
+#    echiquier.append(i)
+#if len(echiquier) == 73:
+#    print(echiquier)
+
+#Exercice 1 fait
+c = "# "
+b = " #"
+echiquier =""
+
+for i in range(0, 8):
+    if (i % 2 == 0):
+        echiquier += b *8 + "\n"
+    else:
+        echiquier += c *8 + "\n"
+print(echiquier)
+
+###exercice 2:
+code=[0, 0, 0, 0]
+a= 0
+b= 0
+c= 0
+d= 0
+e= "------------"
+for i in range(0,4) :
+    code[i] = 1
+    a = code[0]
+    b = code[1]
+    c = code[2]
+    d = code[3]
+    print("{}\n{}\n{}\n{}\n{}".format(a,b,c,d,e))
+    code[i]= 0
+###exercice 3:
+from math import *
+
+def pair(panier):
+    if panier %2 == 0:
+        print("True")
+    else:
+        print("False")
+
+
+
+panier = round(63.4)
+print(panier)
+print(pair(panier))
+
+###exercice 4:
+print("insert number ")
+chiffre= int(input())
+print("le factorielle de ", chiffre, "=")
+while chiffre != 1:
+    chiffre -= 1
+    print("*",chiffre, end='')
